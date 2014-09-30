2c9f4fc45fb99768def87611ea7497c2a807909d
tdcwilliams <tdcwilliams@gmail.com>
2014-09-30 12:17:17 +0200

    rearrange files, change paths

--- startup_local.m
+++ startup_local.m
@@ -1,5 +1,7 @@
 format long;
-addpath dependencies;
-
-disp('TODO: mv SF_polylog.m to dependencies')
-addpath ~/matlab/MyLibrary/GEN_progs/SF_progs
+addpath xtra_funs;
+addpath RTS_progs;
+addpath ND_progs;
+addpath GEN_progs;
+addpath GEN_progs/SF_progs;
+addpath compressive;
