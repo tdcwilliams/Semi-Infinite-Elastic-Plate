8acffc1b7c22172dd80fa69455e0d2d1bed032b0
tdcwilliams <tdcwilliams@gmail.com>
2018-03-27 11:39:02 +0200

    bugs in startup_local.m

--- startup_local.m
+++ startup_local.m
@@ -1,10 +1,13 @@
 format long;
+
+% paths from this repo
 addpath main_funs;
 addpath RTS_progs;
 addpath ND_progs;
+addpath compressive;
 
+% paths from MatlabLibrary
 libdir = getenv('MATLAB_LIB_DIR');
-addpath([libdir '/GEN_progs'];
-addpath([libdir '/GEN_progs/SF_progs'];
-addpath([libdir '/GEN_progs/OP_progs'];
-addpath([libdir '/compressive'];
+addpath([libdir '/GEN_progs']);
+addpath([libdir '/SF_progs']);
+addpath([libdir '/OP_progs']);
