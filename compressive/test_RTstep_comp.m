c2e671d59fbcfcb23e186b312efefbbf0ef01209
tdcwilliams <tdcwilliams@gmail.com>
2018-03-28 16:42:32 +0200

    get test script to work; SUB_RT_Gal_comp.m works now and agrees closely with SUB_RT_Gal_comp_v1.m (R & T slightly different; scattered compressive wave is exactly the same)

--- compressive/test_RTstep_comp.m
+++ compressive/test_RTstep_comp.m
@@ -9,25 +9,12 @@ if 1%%semi-inf plate
    bc = 1;
 end
 
-NN       = [10,1000];
-youngs   = 5.45e9;
+MM = [1,1];
+NN = [10,1000];
+youngs = 5.45e9;
 
-if 1
-   [RT_mats,Ac_scat,y]  = SUB_RTstep_Gal_comp(...
-                           phys_vars,hh,bc,NN,youngs);
-
-   %%scattered flex-grav waves;
-   Rp = RT_mats{1,1};%%1x2 matrix
-   Tp = RT_mats{2,1};%%1x2 matrix
-   Rm = RT_mats{1,2};%%1x2 matrix
-   Tm = RT_mats{2,2};%%1x2 matrix
-
-   %%scattered compressive waves;
-   Acs1p = Ac_scat{1,1};
-   Acs2p = Ac_scat{2,1};
-   Acs1m = Ac_scat{1,2};
-   Acs2m = Ac_scat{2,2};
-end
+[out,y] = SUB_RTstep_Gal_comp(...
+   phys_vars,hh,bc,NN,MM,youngs);
 
 Aci1  = 0;
 Aci2  = 0;
@@ -36,19 +23,33 @@ Ainc2 = 0;
 
 %% run old program with these incident waves
 %% and inputs
-Ai_vec1  = [Aci1,Ainc1].';%%waves from right: [compressive amp,flex-grav amp]
-Ai_vec2  = [Aci2,Ainc2].';%%waves from left
-Ai_vec   = [Ai_vec1,Ai_vec2];
-[R,T]    = SUB_RTstep_Gal_comp_v1(...
-               phys_vars,hh,bc,NN,youngs,Ai_vec);
-RTold    = [R,T]
+Ai_vec2  = [Aci2,Ainc2].';
+ACnew = [0,0];
+if hh(1)==0
+   %%waves from left: [flex-grav amp]
+   Ai_vec1 = Ainc1;
+   Ai_vec   = [0;Ai_vec1;Ai_vec2];
+else
+   %%waves from left: [compressive amp,flex-grav amp]
+   Ai_vec1 = [Aci1,Ainc1].';
+   ACnew(1) = out.Rp(1,:)*Ai_vec1 +...
+               out.Tm(1,:)*Ai_vec2;
+   Ai_vec   = [Ai_vec1;Ai_vec2];
+end
 
-%% display results from new program
-Acs1     = Acs1p*Ai_vec1 + Acs1m*Ai_vec2
-Acs2     = Acs2p*Ai_vec1 + Acs2m*Ai_vec2
-R        = Rp*Ai_vec1    + Rm*Ai_vec2
-T        = Tp*Ai_vec1    + Tm*Ai_vec2
-RTnew    = [R,T]
+ACnew(2) = out.Tp(1,:)*Ai_vec1 +...
+            out.Tm(1,:)*Ai_vec2;
+R        = out.Rp(end,:)*Ai_vec1 +...
+            out.Tm(end,:)*Ai_vec2;
+T        = out.Tp(end,:)*Ai_vec1 +...
+            out.Tm(end,:)*Ai_vec2;
+RTnew    = [R,T];
 
+%%waves from right: [compressive amp,flex-grav amp]
+out0 = SUB_RTstep_Gal_comp_v1(...
+         phys_vars,hh,bc,NN,youngs,Ai_vec);
+ACold = [out0.Ac_scat,out0.Bc_scat]
+ACnew
 
-
+RTold = [out0.R,out0.T]
+RTnew
